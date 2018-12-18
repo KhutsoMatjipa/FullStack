387f1808b4f082a2d07141efa0e0f2135397567d
Khutso Matjipa <khutso@consnet.co.za>
2018-12-18 13:36:40 +0200

    latest

--- server/index.js
+++ server/index.js
@@ -3,6 +3,13 @@ const cors = require('cors');
 const express = require('express');
 const bodyparser = require('body-parser');
 
+const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
 var app = express();
 app.use(bodyparser.json());
 var con = mysql.createConnection({
@@ -13,20 +20,30 @@ var con = mysql.createConnection({
 })
 
 con.connect((err) => {
-    if(!err){
+    if (!err) {
         console.log("DB connection succeded");
     }
     else {
-        console.log("DB connection failed \n Error: "+JSON.stringify(err, undefined, 2));
+        console.log("Connected!");
+        con.query("CREATE DATABASE minedb", function (err, result) {
+            if (err) throw err;
+            console.log("Database created");
+        });
+
+        var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
+        con.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("Table created");
+        });
     }
 });
 
-app.listen(3000, ()=>console.log('Express server is running at port 3000'));
+app.listen(3000, () => console.log('Express server is running at port 3000'));
 
 //GET all Boreholes
-app.get('/boreholes', (req, res)=>{
+app.get('/boreholes', (req, res) => {
     con.query('SELECT * FROM Boreholes', (err, rows, field) => {
-        if(!err) {
+        if (!err) {
             res.send(rows);
         }
         else {
@@ -36,10 +53,10 @@ app.get('/boreholes', (req, res)=>{
 })
 
 //GET a Borehole
-app.get('/borehole/:id', (req, res)=>{
-    con.query('SELECT * FROM Boreholes WHERE borehole_id = ?', [req.params.id] , (err, rows, field) => {
-        if(!err) {
-            res.send(rows); 
+app.get('/borehole/:id', (req, res) => {
+    con.query('SELECT * FROM Boreholes WHERE borehole_id = ?', [req.params.id], (err, rows, field) => {
+        if (!err) {
+            res.send(rows);
         }
         else {
             console.log(err);
@@ -48,9 +65,9 @@ app.get('/borehole/:id', (req, res)=>{
 })
 
 //DELETE a Borehole
-app.delete('/borehole/:id', (req, res)=>{
-    con.query('DELETE FROM Boreholes WHERE borehole_id = ?', [req.params.id] , (err, rows, field) => {
-        if(!err) {
+app.delete('/borehole/:id', (req, res) => {
+    con.query('DELETE FROM Boreholes WHERE borehole_id = ?', [req.params.id], (err, rows, field) => {
+        if (!err) {
             res.send('Deleted successfully');
         }
         else {
